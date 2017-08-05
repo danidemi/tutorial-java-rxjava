e10a325d6c6f0d01d8133eb683130c2e2f317922
Daniele Demichelis <d.demichelis@techgap.it>
2017-08-05 16:56:08 +0200

    Creating observables.

---
+++ src/test/java/com/danidemi/tutorial/java/javarx/CreatingObservables.java
@@ -0,0 +1,69 @@
+package com.danidemi.tutorial.java.javarx;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreatingObservables {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @AfterEach
+    void logTestEnd() {
+        log.info("Test ended.");
+    }
+
+
+    @Test
+    public void createAnObserver() {
+
+        Observable<String> observable = Observable.create(observer -> {
+            for (int i = 0; i < 5; i++) {
+                log.info("emitting '{}'", i);
+                observer.onNext(String.valueOf(i));
+            }
+            observer.onComplete();
+        });
+
+        StringBuffer sb = new StringBuffer();
+        observable.subscribe(next -> {
+            log.info("observing '{}'", next);
+            sb.append(next);
+        });
+
+        assertEquals( "01234", sb.toString() );
+
+    }
+
+    @Test
+    public void anObserverThatEmitsDataAtIntervals() throws InterruptedException {
+
+        Observable<Long> observableTimer = Observable.timer(1500, TimeUnit.MILLISECONDS);
+
+        AtomicLong theLong = new AtomicLong(999L);
+        observableTimer.subscribe(new Consumer<Long>() {
+            @Override public void accept(Long aLong) throws Exception {
+                log.info("observing '{}'", aLong);
+                theLong.set(aLong);
+            }
+        });
+
+        while(theLong.get() != 0L){
+            log.info("waiting");
+            Thread.sleep(250);
+        }
+
+        assertEquals( 0L, theLong.get() );
+
+
+    }
+
+}
